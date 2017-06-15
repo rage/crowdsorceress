647c93e61c23de03dfada1285744cc074495e919
Irene <irene.nikkarinen@helsinki.fi>
2017-06-15 16:55:44 +0300

    Add broadcast to front testing

--- app/channels/submission_status_channel.rb
+++ app/channels/submission_status_channel.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
+require 'json'
 
 class SubmissionStatusChannel < ApplicationCable::Channel
+
   def subscribed
     # stream_from "some_channel"
     stream_from "SubmissionStatus"
@@ -10,4 +12,32 @@ class SubmissionStatusChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  # send current status in case socket opened too late
+  def receive(data)
+    return unless data["ping"]
+    test_subscription
+  end
+
+
+  private
+  def test_subscription
+    data = {
+      'message'=> "pong1",
+      'progress'=> 0,
+    }
+
+    SubmissionStatusChannel.broadcast_to("SubmissionStatus", JSON[data]
+    )
+
+    sleep 15
+
+    data1 = {
+      'message'=> "pong2",
+      'progress'=> 1,
+    }
+    SubmissionStatusChannel.broadcast_to("SubmissionStatus", JSON[data1]
+    )
+  end
+
 end
