d790532308ebee2113235ae8aaac5f8e364c0052
Henrik Nygren <nygrenh@gmail.com>
2017-06-27 15:41:00 +0300

    Configure prodction redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://127.0.0.1:6379/12' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://127.0.0.1:6379/12' }
+  end
 end
