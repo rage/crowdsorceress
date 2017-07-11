8a91384e11101c7a3d4b9350311c705ffd769134
vilmakoo <vilma.l.kangas@helsinki.fi>
2017-07-11 16:35:43 +0300

    Rubocopped

--- lib/sandbox_results_handler.rb
+++ lib/sandbox_results_handler.rb
@@ -15,6 +15,13 @@ class SandboxResultsHandler
   def generate_data_for_frontend(sandbox_status, test_output, token)
     generate_message(sandbox_status, test_output['status'] == 'PASSED', test_output['status'] != 'COMPILE_FAILED', token)
 
+    set_status_and_passed_state(sandbox_status, test_output)
+
+    # Update exercise's sandbox_results
+    @exercise.save!
+  end
+
+  def set_status_and_passed_state(sandbox_status, test_output)
     # Status will be 'finished' if both stub results and model solution results are finished in sandbox
     if @exercise.sandbox_results[:status] == '' || @exercise.sandbox_results[:status] == 'finished'
       @exercise.sandbox_results[:status] = sandbox_status
@@ -22,9 +29,6 @@ class SandboxResultsHandler
 
     # Model solution is passed if test results are passed
     @exercise.sandbox_results[:passed] = true unless test_output['status'] != 'PASSED'
-
-    # Update exercise's sandbox_results
-    @exercise.save!
   end
 
   def test_results(test_output)
@@ -37,7 +41,6 @@ class SandboxResultsHandler
   end
 
   def compile_errors(test_output, token)
-    # Push compile errors into exercise's error messages
     return unless test_output['status'] == 'COMPILE_FAILED'
 
     token == 'STUB' ? (@exercise.error_messages.push 'Tehtäväpohja ei kääntynyt: ') : (@exercise.error_messages.push 'Malliratkaisu ei kääntynyt: ')
@@ -51,18 +54,26 @@ class SandboxResultsHandler
   # Generate message that will be sent to frontend
   def generate_message(sandbox_status, passed, compiled, token)
     if token == 'MODEL'
-      @exercise.sandbox_results[:message] += ' Malliratkaisun tulokset: '
-      @exercise.sandbox_results[:model_results_received] = true
-      @exercise.sandbox_results[:message] += if sandbox_status == 'finished' && passed then 'Kaikki OK.'
-                                             elsif sandbox_status == 'finished' && compiled then 'Testit eivät menneet läpi.'
-                                             else 'Koodi ei kääntynyt.'
-                                             end
+      model_message(sandbox_status, passed, compiled)
     elsif token == 'STUB'
-      @exercise.sandbox_results[:message] += ' Tehtäväpohjan tulokset: '
-      @exercise.sandbox_results[:stub_results_received] = true
-      @exercise.sandbox_results[:message] += if sandbox_status == 'finished' && compiled then 'Kaikki OK.'
-                                             else 'Koodi ei kääntynyt.'
-                                             end
+      stub_message(sandbox_status, compiled)
     end
+  end
+
+  def model_message(sandbox_status, passed, compiled)
+    @exercise.sandbox_results[:message] += ' Malliratkaisun tulokset: '
+    @exercise.sandbox_results[:model_results_received] = true
+    @exercise.sandbox_results[:message] += if sandbox_status == 'finished' && passed then 'Kaikki OK.'
+                                           elsif sandbox_status == 'finished' && compiled then 'Testit eivät menneet läpi.'
+                                           else 'Koodi ei kääntynyt.'
+                                           end
+  end
+
+  def stub_message(sandbox_status, compiled)
+    @exercise.sandbox_results[:message] += ' Tehtäväpohjan tulokset: '
+    @exercise.sandbox_results[:stub_results_received] = true
+    @exercise.sandbox_results[:message] += if sandbox_status == 'finished' && compiled then 'Kaikki OK.'
+                                           else 'Koodi ei kääntynyt.'
+                                           end
   end
 end
