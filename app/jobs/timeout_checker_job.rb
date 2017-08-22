ce34e8313bc83cc4838ac024cab41f63f8d2e911
Nea <nea.pirttinen@cs.helsinki.fi>
2017-08-22 14:21:45 +0300

    Fix various messages

--- app/jobs/timeout_checker_job.rb
+++ app/jobs/timeout_checker_job.rb
@@ -11,7 +11,7 @@ class TimeoutCheckerJob < ApplicationJob
   # Timeout happens if exercise results are not received fast enough
   def perform(exercise)
     return if exercise.finished? || exercise.error?
-    exercise.error_messages.push('Timeout')
+    exercise.error_messages.push('Yritä lähettää tehtävä uudelleen')
     exercise.sandbox_timeout!
     MessageBroadcasterJob.perform_now(exercise)
   end
