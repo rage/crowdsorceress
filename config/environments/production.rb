b3d94914c4291cda20cc844efacd23f5f5a3bf50
Henrik Nygren <nygrenh@gmail.com>
2017-08-23 16:04:10 +0300

    Update sidekiq production database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,10 +83,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://127.0.0.1:6379/12' }
+    config.redis = { url: 'redis://127.0.0.1:6379/0' }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://127.0.0.1:6379/12' }
+    config.redis = { url: 'redis://127.0.0.1:6379/0' }
   end
 end
