c22f6f8f134e509d8ebb1381b35b38fccaee6c2f
Nea <nea.pirttinen@cs.helsinki.fi>
2017-09-25 16:02:50 +0300

    Fix peer review tests

--- spec/integration/api/v0/assignments/peer_review_exercise_controller_spec.rb
+++ spec/integration/api/v0/assignments/peer_review_exercise_controller_spec.rb
@@ -5,12 +5,18 @@ require 'rails_helper'
 RSpec.describe Api::V0::Assignments::PeerReviewExerciseController, type: :request do
   let(:user) { FactoryGirl.create(:user) }
   before { allow(controller).to receive(:current_user) { user } }
-  let(:exercise) { FactoryGirl.create(:exercise, user: user) }
+  before { allow_any_instance_of(Api::V0::BaseController).to receive(:ensure_signed_in!) }
+  let(:exercise) { FactoryGirl.create(:exercise, user: user, status: 'finished') }
 
   it 'assigns exercises for reviewing' do
-    exercise.update status: 'finished'
-    get "/api/v0/assignments/#{exercise.assignment.id}/peer_review_exercise?count=3"
+    get "/api/v0/assignments/#{exercise.assignment_id}/peer_review_exercise?count=3"
     expect(response.status).to eq(200)
+  end
+
+  it 'assigns an exercise for self review when possible' do
+    get "/api/v0/assignments/#{exercise.assignment_id}/peer_review_exercise?count=3"
+    expect(response.status).to eq(200)
+    expect(response.body).to include(user.id.to_s)
   end
 
   it 'handles error when requesting an exercise for assignment that has no finished exercises' do
