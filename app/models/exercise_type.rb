3a6832da863c87fe7612a3f96f67c9caa478c83e
vilmakoo <vilma.l.kangas@helsinki.fi>
2018-04-23 15:31:17 +0300

    Fix #40

--- app/models/exercise_type.rb
+++ app/models/exercise_type.rb
@@ -3,4 +3,12 @@
 class ExerciseType < ApplicationRecord
   has_many :exercises
   has_many :peer_review_questions
+
+  def trim_input_and_output_types
+    input_type.strip!
+    output_type.strip!
+
+    update(input_type: 'String') if input_type == 'string'
+    update(output_type: 'String') if output_type == 'string'
+  end
 end
