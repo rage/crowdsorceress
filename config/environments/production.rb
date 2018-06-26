63604dc1b3ec86a35a8daaa60178f89a098dd9a5
Henrik Nygren <nygrenh@gmail.com>
2018-06-26 13:46:34 +0300

    Allow demo to allowed origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = ['http://localhost:4567', 'http://127.0.0.1:4567', 'http://localhost:3001',
-                                                 'https://crowdsorcerer.testmycode.io/', 'https://materiaalit.github.io']
+  config.action_cable.allowed_request_origins = ['http://localhost:4567', 'http://127.0.0.1:4567', 'http://localhost:3001', 'https://crowdsorcerer.testmycode.io/', 'https://materiaalit.github.io', 'https://crowdsorcerer-demo.now.sh']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
