c6f0f68a38affc3352f56df6a3f638c4e0879576
vilmakoo <vilma.l.kangas@helsinki.fi>
2018-09-28 15:25:54 +0300

    Send testing type to front end for peer reviewing

--- app/controllers/api/v0/assignments/peer_review_exercise_controller.rb
+++ app/controllers/api/v0/assignments/peer_review_exercise_controller.rb
@@ -15,7 +15,18 @@ module Api
           raise NoExerciseError if exercises.empty? || exercises.first.nil?
 
           pr_questions = exercises.first.assignment.exercise_type.peer_review_questions
-          render json: { exercises: exercises, peer_review_questions: pr_questions, tags: Tag.recommended }
+          render json: { exercises: exercises, peer_review_questions: pr_questions,
+                         tags: Tag.recommended, testing_type: testing_type(assignment.exercise_type) }
+        end
+
+        def testing_type(exercise_type)
+          if exercise_type.testing_type == 'input_output'
+            'input_output'
+          elsif exercise_type.testing_type == 'student_written_tests'
+            'unit_tests'
+          elsif exercise_type.testing_type == 'io_and_code'
+            'io_and_code'
+          end
         end
       end
     end
